4c4ffc8e74b6c02083bfb39f8e8979f5ef304048
Stacy Areas <stacyareas@gmail.com>
2018-03-15 16:56:45 -0700

    bootstrap index

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require("express");
+var exphbs = require("express-handlebars");
 var bodyParser = require("body-parser");
 var logger = require("morgan");
 var mongoose = require("mongoose");
@@ -16,51 +17,54 @@ app.use(logger("dev"));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
 mongoose.Promise = Promise;
 mongoose.connect("mongodb://localhost/newsScraperdb", {useMongoClient: true});
 
 app.get("/", function(req, res) {
-	res.send(index.html);
+	res.redirect("/articles");
 });
 
 app.get("/scrape", function(req, res) {
 	axios.get("https://www.vice.com/en_us").then(function(response) {
 		var $ = cheerio.load(response.data);
-		console.log("I'm here");
-		console.log($(".grid__wrapper__card").length);
-		$(".grid__wrapper__card").each(function(i, element) {
-			var result = {};
 
-			result.title = $(this).find(".grid-card-linkout--site-name").text();
-			result.link = $(this).attr("href");
-			console.log("result.topic", result.title);
-			console.log("result.topic", result.link);
-			// result.title = $(this).children("")
-			db.Article.create(result).then(function(dbArticle) {
+		$(".grid__wrapper__card").each(function(i, element) {
+
+			if (result.title && result.link) {
+
+				var result = {};
+
+				result.title = $(this).find(".grid-card-linkout--site-name").text();
+				result.link = $(this).attr("href");
+			
+				db.Article.create(result).then(function(dbArticle) {
 				console.log(dbArticle);
-			}).catch(function(err) {
+				}).catch(function(err) {
 				console.log(err);
-				// return res.json(err);
-			});
+				});
+			}
 		});
-		res.send("Scraped");
+		res.redirect("/articles")
 	});
 });
 
 app.get("/articles", function(req, res) {
 	db.Article.find({}).then(function(dbArticle) {
-		res.json(dbArticle);
+		res.render("index", {result: dbArticle});
 	}).catch(function(err) {
-		res.json(err);
+		console.log(err);
 	});
 });
 
 app.get("/articles/:id", function(req, res) {
 	db.Article.findOne({_id: req.params.id})
 	.populate("comment").then(function(dbArticle) {
-		res.json(dbArticle);
+		res.render("comments", {result: dbArticle});
 	}).catch(function(err) {
-		res.json(err);
+		console.log(err);
 	});
 });
 
@@ -68,9 +72,9 @@ app.post("/articles/:id", function(req, res) {
 	db.Comment.create(req.body).then(function(dbComment) {
 		return db.Article.findOneAndUpdate({_id: req.params.id}, {comment: dbComment._id}, {new: true});
 	}).then(function(dbArticle) {
-		res.json(dbArticle);
+		console.log(dbArticle);
 	}).catch(function(err) {
-		res.json(err);
+		console.log(err);
 	});
 });
 
